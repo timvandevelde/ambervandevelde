3fbddb0c24df6d2f9b24e3f4914d9fc0812f9b9b
Tim van de Velde <timvandevelde@voormedia.com>
2018-02-12 09:25:35 +0100

    Services and content social media

--- page.php
+++ page.php
@@ -7,8 +7,6 @@
         <!-- <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?> -->
         <?php the_content(); ?>
 
-
-
         <div class="entry-links"><?php wp_link_pages(); ?></div>
       </section>
     </article>
