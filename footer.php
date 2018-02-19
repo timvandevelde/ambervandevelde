d48d33a9f2d7f54da111609ec4f44dea0be49821
Tim van de Velde <timvandevelde@voormedia.com>
2018-02-19 22:21:33 +0100

    IE11 fixes

--- footer.php
+++ footer.php
@@ -51,5 +51,15 @@
 
   gtag('config', 'UA-9509831-13');
 </script>
+<script>
+  var $buoop = {notify:{i:-3,f:-4,o:-4,s:-2,c:-4},insecure:true,api:5};
+  function $buo_f(){
+   var e = document.createElement("script");
+   e.src = "//browser-update.org/update.min.js";
+   document.body.appendChild(e);
+  };
+  try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
+  catch(e){window.attachEvent("onload", $buo_f)}
+</script>
 </body>
 </html>
