3e2ace82bce1a9542e08585284aaaf84dc52c9ec
Tim van de Velde <timvandevelde@voormedia.com>
2018-02-21 23:11:47 +0100

    Favicon to header

--- header.php
+++ header.php
@@ -3,6 +3,13 @@
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
   <meta name="viewport" content="width=device-width" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="theme-color" content="#ffffff">
   <link href="https://file.myfontastic.com/vSGh3i7VxFLbTCSfQSWkvk/icons.css" rel="stylesheet">
   <?php wp_head(); ?>
   <script src="<?php echo get_stylesheet_directory_uri(); ?>/scripts/scrollreveal.min.js" type="text/javascript"></script>
